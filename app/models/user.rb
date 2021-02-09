281013a9c5a322351fa3bc44a31da09a06a724f0
yuyafujiwara <yuya.0509f@gmail.com>
2021-02-09 10:55:03 +0900

    user login

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :profile, length: { maximum: 200 }
+
+  attr_accessor :current_password
+  mount_uploader :image, ImageUploader
+
+end
